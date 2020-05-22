1333c9cebba563c46e58ee9de8283b7b24cc1c88
Eber Kachi <eber.nina96@gmail.com>
2020-05-22 02:46:27 -0400

    buscador empresa

--- Modelo/DBEmpresa.php
+++ Modelo/DBEmpresa.php
@@ -1,150 +1,184 @@
 <?php
-    require_once("../Modelo/Conexion.php");
-    class DBEmpresa
+require_once("../Modelo/Conexion.php");
+
+class DBEmpresa
+{
+    private $conexion;
+
+    public function __construct()
     {
-        private $conexion;
+        $this->conexion = new Conexion();
+    }
 
-        public function __construct()
-        {
-            $this->conexion =  new Conexion();
-        }
-
-        public function listaDeEmpresas()
-        {  
-            $sqlListaDeEmpresas = "SELECT * 
+    public function listaDeEmpresas()
+    {
+        $sqlListaDeEmpresas = "SELECT * 
             FROM empresa
             ORDER BY nombre asc;";
-            $cmd = $this->conexion->prepare($sqlListaDeEmpresas);
-            $cmd->execute();
-            $listaDeEmpresasConsulta = $cmd->fetchAll();
-            return $listaDeEmpresasConsulta;
-        }
-        
-        public function registrarEmpresa($idEmpresa,$nit,$nombre,$direccion,$cantidadProyectos,$logo, $telefono,$descripcion, $activo)
-        {
-            $sqlInsertarEmpresa= "
+        $cmd = $this->conexion->prepare($sqlListaDeEmpresas);
+        $cmd->execute();
+        $listaDeEmpresasConsulta = $cmd->fetchAll();
+        return $listaDeEmpresasConsulta;
+    }
+
+    public function registrarEmpresa($idEmpresa, $nit, $nombre, $direccion, $cantidadProyectos, $logo, $telefono, $descripcion, $activo)
+    {
+        $sqlInsertarEmpresa = "
             INSERT INTO empresa(idEmpresa,nit,nombre,direccion,logo,telefono,descripcion,activo)
             VALUES(:idEmpresa,:nit,:nombre,:direccion,:logo,:telefono,:descripcion,:activo);";
 
-            try {
-                $cmd = $this->conexion->prepare($sqlInsertarEmpresa);
-                $cmd->bindParam(':idEmpresa', $idEmpresa);
-                $cmd->bindParam(':nit', $nit);
-                $cmd->bindParam(':nombre', $nombre);
-                $cmd->bindParam(':direccion', $direccion);
-                $cmd->bindParam(':logo', $logo);
-                $cmd->bindParam(':telefono', $telefono);
-                $cmd->bindParam(':descripcion', $descripcion);
-                $cmd->bindParam(':activo', $activo);
-                if ($cmd->execute()) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } catch (PDOException $e) {
-                echo 'ERROR: No se logro realizar la nueva inserción - '.$e->getMesage();
-                exit();
-                return 0;
-            }
-        }
-//validando el nit de empresa 
-        public function ValidarNit($nit)
-        {
-            $sqlValidarNit = "SELECT * FROM empresa WHERE nit = :nit";
-            $cmd = $this->conexion->prepare($sqlValidarNit);
+        try {
+            $cmd = $this->conexion->prepare($sqlInsertarEmpresa);
+            $cmd->bindParam(':idEmpresa', $idEmpresa);
             $cmd->bindParam(':nit', $nit);
-            $cmd->execute();
-
-            $resultado = $cmd->fetch();
-
-            if ($resultado) {
+            $cmd->bindParam(':nombre', $nombre);
+            $cmd->bindParam(':direccion', $direccion);
+            $cmd->bindParam(':logo', $logo);
+            $cmd->bindParam(':telefono', $telefono);
+            $cmd->bindParam(':descripcion', $descripcion);
+            $cmd->bindParam(':activo', $activo);
+            if ($cmd->execute()) {
                 return 1;
             } else {
                 return 0;
             }
-        }   
- //recuperando datos del departamento pra actualizar
-        public function datosEmpresa($idEmpresa)
-        {
-            $sqlDatosEmpresa = "SELECT * FROM empresa WHERE idEmpresa = :idEmpresa;";
-            $cmd = $this->conexion->prepare($sqlDatosEmpresa);
-            $cmd->bindParam(':idEmpresa', $idEmpresa);
-            $cmd->execute();
-
-            $datosEmpresa = $cmd->fetch();
-
-            if ($datosEmpresa) {
-                return $datosEmpresa;
-            } else {
-                return null;
-            }
+        } catch (PDOException $e) {
+            echo 'ERROR: No se logro realizar la nueva inserción - ' . $e->getMesage();
+            exit();
+            return 0;
         }
-        public function ActualizarEmpresa($idEmpresa,$nit,$nombre,$direccion,$logo,$telefono,$descripcion,$activo)
-        {
-            $sqlActualizarEmpresa = " UPDATE empresa 
+    }
+
+//validando el nit de empresa
+    public function ValidarNit($nit)
+    {
+        $sqlValidarNit = "SELECT * FROM empresa WHERE nit = :nit";
+        $cmd = $this->conexion->prepare($sqlValidarNit);
+        $cmd->bindParam(':nit', $nit);
+        $cmd->execute();
+
+        $resultado = $cmd->fetch();
+
+        if ($resultado) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    //recuperando datos del departamento pra actualizar
+    public function datosEmpresa($idEmpresa)
+    {
+        $sqlDatosEmpresa = "SELECT * FROM empresa WHERE idEmpresa = :idEmpresa;";
+        $cmd = $this->conexion->prepare($sqlDatosEmpresa);
+        $cmd->bindParam(':idEmpresa', $idEmpresa);
+        $cmd->execute();
+
+        $datosEmpresa = $cmd->fetch();
+
+        if ($datosEmpresa) {
+            return $datosEmpresa;
+        } else {
+            return null;
+        }
+    }
+
+    public function ActualizarEmpresa($idEmpresa, $nit, $nombre, $direccion, $logo, $telefono, $descripcion, $activo)
+    {
+        $sqlActualizarEmpresa = " UPDATE empresa 
             SET nit=:nit,nombre=:nombre,direccion=:direccion,logo=:logo,telefono=:telefono,descripcion=:descripcion,activo=:activo 
             WHERE idEmpresa=:idEmpresa;";
-            try {
-                $cmd = $this->conexion->prepare($sqlActualizarEmpresa);
-                $cmd->bindParam(':idEmpresa', $idEmpresa);
-                $cmd->bindParam(':nit', $nit);
-                $cmd->bindParam(':nombre', $nombre);
-                $cmd->bindParam(':direccion', $direccion);
-                $cmd->bindParam(':logo', $logo);
-                $cmd->bindParam(':telefono', $telefono);
-                $cmd->bindParam(':descripcion', $descripcion);
-                $cmd->bindParam(':activo', $activo);
-                if ($cmd->execute()) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } catch (PDOException $e) {
-                echo 'ERROR: No se logro actualizar los datos de la empreza - '.$e->getMesage();
-                exit();
+        try {
+            $cmd = $this->conexion->prepare($sqlActualizarEmpresa);
+            $cmd->bindParam(':idEmpresa', $idEmpresa);
+            $cmd->bindParam(':nit', $nit);
+            $cmd->bindParam(':nombre', $nombre);
+            $cmd->bindParam(':direccion', $direccion);
+            $cmd->bindParam(':logo', $logo);
+            $cmd->bindParam(':telefono', $telefono);
+            $cmd->bindParam(':descripcion', $descripcion);
+            $cmd->bindParam(':activo', $activo);
+            if ($cmd->execute()) {
+                return 1;
+            } else {
                 return 0;
             }
+        } catch (PDOException $e) {
+            echo 'ERROR: No se logro actualizar los datos de la empreza - ' . $e->getMesage();
+            exit();
+            return 0;
+        }
+    }
+
+
+    public function EliminarEmpresa($idEmpresa)
+    {
+        $sqlEliminarEmpresa = "DELETE FROM empresa WHERE idEmpresa = :idEmpresa;";
+
+        try {
+            $cmd = $this->conexion->prepare($sqlEliminarEmpresa);
+            $cmd->bindParam(':idEmpresa', $idEmpresa);
+
+            if ($cmd->execute()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (PDOException $e) {
+            echo 'ERROR: No se pudo eliminar los datos de la empresa' . $e->getMesage();
+        }
+    }
+
+    //buscador   de empresas
+    public function buscadorEmpresa($nombre, $nit, $activo, $cproyecto )
+    {
+        $consulta = "SELECT e.* , p.nombre as proyectoNombre,COUNT(p.idProyecto) AS totalProyecto  FROM empresa as e left JOIN proyecto p on e.idEmpresa= p.idEmpresa";
+        $activoVal = '';
+        if ($activo == 'i') {
+            $activoVal = "0";
+        } elseif ($activo == '1') {
+            $activoVal = "1";
         }
 
-  
-        public function EliminarEmpresa($idEmpresa)
-        {
-            $sqlEliminarEmpresa = "DELETE FROM empresa WHERE idEmpresa = :idEmpresa;";
-
-            try {
-                $cmd = $this->conexion->prepare($sqlEliminarEmpresa);
-                $cmd->bindParam(':idEmpresa', $idEmpresa);
-
-                if ($cmd->execute()) {
-                    return 1;
-                } else {
-                    return 0;
+        if ($nombre && $nit && $activo) {
+            $consulta = "SELECT e.* , p.nombre as proyectoNombre,COUNT(p.idProyecto) AS totalProyecto FROM empresa as e left JOIN proyecto p on e.idEmpresa= p.idEmpresa
+             WHERE e.nombre LIKE '%$nombre%' or 
+             e.nit LIKE '%$nit%' and 
+             e.activo = $activoVal
+             ";
+        } elseif ($nombre && $nit) {
+            $consulta = "SELECT e.* , p.nombre as proyectoNombre ,COUNT(p.idProyecto) AS totalProyecto FROM empresa as e left JOIN proyecto p on e.idEmpresa= p.idEmpresa
+             WHERE e.nombre LIKE '%$nombre%' or 
+               e.nit LIKE '%$nit%' ";
+        } elseif ($nit) {
+            $consulta .= " WHERE e.nit LIKE '%$nit%'";
+        } elseif ($nombre) {
+            $consulta .= " WHERE e.nombre LIKE '%$nombre%'";
+        } elseif ($activo == 'i') {
+            $consulta .= " WHERE e.activo = 0";
+        } elseif ($activo == '1') {
+            $consulta .= " WHERE e.activo = 1";
+        }
+        $consulta .= ' GROUP BY e.idEmpresa ORDER BY e.nombre ASC';
+        $cmd = $this->conexion->prepare($consulta);
+//        $cmd->bindParam(':nombre', $nombre);
+        $cmd->execute();
+        $listaEmpresa = $cmd->fetchAll();
+        $nuevaLista=array();
+        if ($cproyecto>0) {
+            foreach ($listaEmpresa as $empresa){
+                if($empresa['totalProyecto']>=$cproyecto){
+                    array_push($nuevaLista, $empresa);
                 }
-            } catch (PDOException $e) {
-                echo 'ERROR: No se pudo eliminar los datos de la empresa'.$e->getMesage();
             }
-        }  
-  //buscador   de empresas   
-        public function buscadorEmpresa($nombre)
-        {
-            $consulta="SELECT * FROM empresa"; 
-
-            $cmd = $this->conexion->prepare($consulta);
-
-            if($nombre){
-                $consulta+= "WHERE nombre LIKE '%:nombre%'";
-                $cmd->bindParam(':nombre', $nombre);
-            }
-
-
-            $listaEmpresa= $cmd->fetch();
-            return $listaEmpresa;
+            return $nuevaLista;
         }
-  
-        
 
-    }//end class
-     
-    
+        return $listaEmpresa;
+    }
+
+
+}//end class
+
 
 ?>
--- Vista/IUListaEmpresas.php
+++ Vista/IUListaEmpresas.php
@@ -7,14 +7,16 @@ $listaEmpresas = $objLNListaEmpresa->listaDeEmpresas();
 
 $objModeloEmpresa = new DBEmpresa();
 
-if(isset($_REQUEST['nombre']) || isset($_REQUEST['nit']) || isset($_REQUEST['cproyecto'])){
-  $nombre=$_REQUEST['nombre'];
+if (isset($_REQUEST['nombre']) || isset($_REQUEST['nit']) || isset($_REQUEST['cproyecto'])|| isset($_REQUEST['estado'])) {
+    $nombre = ($_REQUEST['nombre'])?$_REQUEST['nombre'] : '';
+    $nit = ($_REQUEST['nit'])? $_REQUEST['nit']: '';
 
-  // $consulta += "WHERE nombre LIKE '%{$nombre}%'";
-  $listaEmpresas= $objModeloEmpresa->buscadorEmpresa($nombre);
+    $activo = $_REQUEST['estado'];
+
+    // $consulta += "WHERE nombre LIKE '%{$nombre}%'";
+    $listaEmpresas = $objModeloEmpresa->buscadorEmpresa($nombre,$nit,$activo,$_REQUEST['cproyecto']);
 
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -23,97 +25,106 @@ if(isset($_REQUEST['nombre']) || isset($_REQUEST['nit']) || isset($_REQUEST['cpr
 <head>
     <title>Lista de Empresa</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
 <body style="background-color:##C8F7F1;">
-    <h1>
-        <font color="black">Lista de Empresa</font>
-    </h1>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
 
-
-    <table border=2>
-        <form action="" method="post">
-            <div class="row">
-                <div class="col-6">
-                    <div class="form-row">
-                        <div class="form-group col-md-6 form-inline">
-                            <label>Nombre </label>
-                            <input type="text" class="form-control" name="nombre">
-                        </div>
-                        <div class="form-group col-md-6 form-inline">
-                            <label>Nit</label>
-                            <input type="text" class="form-control" name="nit">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="form-row">
-                        <div class="form-group col-md-6 form-inline">
-                            <label>Cantidad de proyectos </label>
-                            <input type="text" class="form-control" name="cproyecto">
+            <h1>
+                <font color="black">Lista de Empresa</font>
+            </h1>
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-row">
+                            <div class="form-group col-md-6 form-inline">
+                                <label>Nombre </label>
+                                <input type="text" class="form-control" name="nombre">
+                            </div>
+                            <div class="form-group col-md-6 form-inline">
+                                <label>Nit</label>
+                                <input type="text" class="form-control" name="nit">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="form-row">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" id="inlineCheckbox2" name="estado"
-                                value="activos">
-                            <label class="form-check-label" for="inlineCheckbox2">Activos</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" id="inlineCheckbox2" name="estado"
-                                value="inactivos">
-                            <label class="form-check-label" for="inlineCheckbox2">inactivos</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" id="inlineCheckbox2" checked name="estado"
-                                value="todos">
-                            <label class="form-check-label" for="inlineCheckbox2">todos</label>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-row">
+                            <div class="form-group col-md-6 form-inline">
+                                <label>Cantidad de proyectos </label>
+                                <input type="number" class="form-control" min="0" max="100" name="cproyecto" value="0">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Buscar</button>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" id="inlineCheckbox2" name="estado" value="1">
+                                <label class="form-check-label" for="inlineCheckbox2">Activos</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" id="inlineCheckbox2" name="estado" value="i"> <!--no agarra 0 en el input-->
+                                <label class="form-check-label" for="inlineCheckbox2">inactivos</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" id="inlineCheckbox2" checked name="estado" value="" >
+                                <label class="form-check-label" for="inlineCheckbox2">todos</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </form>
-        <h3 align="center" title="Este es un Enlace a Listar Empresas"> <a href="./IURegistrarEmpresa.php">Registrar</a>
-        </h3>
-        <tr>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Buscar</button>
+                    </div>
+                </div>
+            </form>
+            <h3 align="center" title="Este es un Enlace a Listar Empresas"><a href="./IURegistrarEmpresa.php">Registrar</a>
+            </h3>
 
-            <th>Logo</th>
-            <th>Nombre</th>
-            <th>Telefono</th>
-            <th>Direccion</th>
-            <th>Estado</th>
+            <table class="table">
+                <thead>
+                <tr>
 
-        </tr>
+                    <th>Logo</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Direccion</th>
+                    <th>Estado</th>
 
-        <?php
-     foreach ($listaEmpresas as $registroEmpresas) {
-         ?>
-        <tr>
-            <td><?php echo $registroEmpresas['logo']; ?></td>
-            <td><?php echo $registroEmpresas['nombre']; ?></td>
-            <td><?php echo $registroEmpresas['telefono']; ?></td>
-            <td><?php echo $registroEmpresas['direccion']; ?></td>
-            <td><?php echo $registroEmpresas['activo']; ?></td>
- 
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($listaEmpresas as $registroEmpresas) {
+                    ?>
+                    <tr>
+                        <!--                        <td>--><?php //echo $registroEmpresas['logo']; ?><!--</td>-->
+                        <td><?php echo $registroEmpresas['nombre']; ?></td>
+                        <td><?php echo $registroEmpresas['telefono']; ?></td>
+                        <td><?php echo $registroEmpresas['direccion']; ?></td>
+                        <td><?php echo $registroEmpresas['activo']; ?></td>
 
-            <td><a href="../Vista/IUActualizarEmpresa.php?idEmpresa=<?php  echo $registroEmpresas['idEmpresa']; ?>">Actualizar</a>
-            <td><a href="../Logica/LNEliminarEmpresa.php?idEmpresa=<?php  echo $registroEmpresas['idEmpresa']; ?>">Eliminar</a>
+
+                        <td><a href="../Vista/IUActualizarEmpresa.php?idEmpresa=<?php echo $registroEmpresas['idEmpresa']; ?>">Actualizar</a>
+                        <td><a href="../Logica/LNEliminarEmpresa.php?idEmpresa=<?php echo $registroEmpresas['idEmpresa']; ?>">Eliminar</a>
 
 
-        </tr>
+                    </tr>
 
-        <?php
-     } ?>
-        <h3 title="Volver al inicio"> <a href="./index.php">Inicio</a></h3>
+                    <?php
+
+                } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!--    <h3 title="Volver al inicio"><a href="./index.php">Inicio</a></h3>-->
